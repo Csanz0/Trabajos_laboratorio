9a1fe376868f1eda0af6bcef15be0e1a28c07016
Carlos Sanzonetty <sanzonettycar@gmail.com>
2022-05-04 13:25:46 -0400

    Adding other modal

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
-<?php require_once('./includes/header.php')?>
+<?php require_once('./includes/header.php');
+
+
+?>
 <header class="head">
     <h1>Libros añadidos</h1>
     <h2><a href="crear.php">Añadir nuevo libro</a></h2>
 </header>
 <main>
-    <?php if($_GET['message']){?>
+    <?php if(isset($_GET['message'])){?>
         <div class="modal">
-            <p><?php echo $_GET['message']?></p>
+            <p class="message"><?php echo $_GET['message']?></p>
             <button id="close">Cerrar</button>
         </div>
-    
         <?php } ?>
+    <?php if(isset($_GET['error'])){?>
+        <div class="modal">
+            <p class="error"><?php echo $_GET['error']?></p>
+            <button id="close">Cerrar</button>
+        </div>
+        <?php } ?>
+ 
     <section class="booksView">
         <?php if(!empty($BOOKS)){?>
 
