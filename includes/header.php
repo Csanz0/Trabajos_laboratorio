8df69d63aef1247a040d6863589fff6753b891e4
Carlos Sanzonetty <sanzonettycar@gmail.com>
2022-05-04 12:17:34 -0400

    adding the folder includes

---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<?php 
+   date_default_timezone_set('America/Caracas');
+   define('BOOKS',[]);
+
+   Class Books{
+       public $name;
+       public $comment;
+       
+            
+       function getBook($name, $comment){
+            $this->name = $name;
+            $this->comment = $comment;
+            $BOOKS[] = $this->name;
+        } 
+
+           function saveBook(){
+               setcookie($this->name, $this->comment, 0, "/");
+           }
+
+           function deleteBook(){
+               unset($_COOKIE[$this->name]);
+               setcookie($this->name, null,-1, "/");
+           }
+        } 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Itinerario de Libros</title>
+</head>
+<body>
+    
