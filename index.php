54c5b3b93914ddedd0f799c6cdce056c61a72c22
Carlos Sanzonetty <sanzonettycar@gmail.com>
2022-05-04 12:19:02 -0400

    adding the index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php require_once('./includes/header.php')?>
+<header class="head">
+    <h1>Libros añadidos</h1>
+    <h2><a href="crear.php">Añadir nuevo libro</a></h2>
+</header>
+<main>
+    <section class="booksView">
+        <?php if(!empty($BOOKS)){?>
+
+                <?php foreach($BOOKS as $bookName){?>
+                    <article class="bookCard">
+                        <div class="article-div">
+                            <h3><?php echo $_COOKIE[$bookName]?></h3>
+                            <p class="comment">
+                                <?php echo $_COOKIE['value'] ?>
+                            </p>
+                        </div>
+                    </article>
+
+                 <?php } ?>
+            <?php }else{ ?>
+            <?php echo ""; }?>    
+    </section>    
+</main>
+<?php require_once('./includes/footer.php')?>
