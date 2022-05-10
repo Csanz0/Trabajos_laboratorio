d56ba3a0cf09a1fb2cecabbbc52066dbe02da0a4
Carlos Sanzonetty <sanzonettycar@gmail.com>
2022-05-10 14:31:26 -0400

    Create a file that contains my vars

---
+++ Actividad_1.2/includes/vars.php
@@ -0,0 +1,23 @@
+<?php 
+
+    define('BOOKS',[]);
+
+    $libro1 = new Book('El principito',"fantasia, aventura",20);
+
+    $libro2 = new Book('El señor de los anillos',"fantasia, aventura",40);
+
+    $libro3 = new Book('El hobbit',"fantasia, aventura",30);
+
+    $libro4 = new Book('It',"Terror",55);
+
+    $libro5 = new Book('Dracula',"Terror",60);
+
+    $BOOKS[] = $libro1;
+    $BOOKS[] = $libro2;
+    $BOOKS[] = $libro3;
+    $BOOKS[] = $libro4;
+    $BOOKS[] = $libro5;
+
+
+
+?>
